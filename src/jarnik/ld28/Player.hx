880cca0b46dcf6b8b89d75ee0671f820542a0464
Jaroslav Meloun <jaroslav.meloun@gmail.com>
2013-12-14 18:39:53 +0100

    stats

--- src/jarnik/ld28/Player.hx
+++ src/jarnik/ld28/Player.hx
@@ -1,21 +1,51 @@
 package jarnik.ld28;
+import nme.net.URLLoader;
+import nme.events.Event;
+import nme.net.URLRequest;
 
 /**
  * ...
  * @author Jaroslav Meloun - jarnik - www.jarnik.com
  */
+
+ typedef STAT = {
+	id:String,
+	votes:Int,
+	percent:Float
+ }
+ 
 class Player
 {
 	
 	public static var playerChoice:String;
+	private static var handler:Void->Void;
+	private static var loader:URLLoader;
+	private static var stats:Array<STAT>;
 
 	public static function init():Void {
 		playerChoice = "OFFER COIN TO HOBO";
+		loader = new URLLoader();
+		loader.addEventListener( Event.COMPLETE, onLoaded );
+		stats = [];
 	}
 	
-	public function new() 
-	{
+	public function new() {
 		
 	}
+	
+	public static function getStats( vote:String, handler:Void->Void ):Void {
+		Player.handler = handler;
+		var urlRequest:URLRequest = new URLRequest("http://jarnik.com/pub/ld28/vote.php");
+		urlRequest.method = 'POST';
+		if ( vote != null && vote != "" )
+			urlRequest.data.id = vote;
+		loader.load( urlRequest );
+	}
+	
+	private static function onLoaded( e:Event ):Void {
+		trace( "LOADED "+loader.data );
+		handler();
+	}
+	
 	
 }
--- src/jarnik/ld28/StatScene.hx
+++ src/jarnik/ld28/StatScene.hx
@@ -10,38 +10,69 @@ import gaxe.Gaxe;
  * ...
  * @author Jaroslav Meloun - jarnik - www.jarnik.com
  */
+
+enum PlaySceneState {
+    STATS_LOADING;
+    STATS_SHOW;
+}
+ 
 class StatScene extends Scene
 {
 	
 	private var screen:RenderGroupStates;
+	private var stats:Array<RenderGroupStates>;
 
 	override private function create():Void 
 	{
 		super.create();
 		
-		screen = Render.renderGroupStates("street");
+		screen = Render.renderGroupStates("stats");
 		addChild( screen );
 		
-		var popup:RenderGroupStates = cast( screen.fetch("popup"), RenderGroupStates );
-		popup.switchState("stats", true );
-		popup.visible = true;
+		stats = [];
+		for ( i in 0...6 )
+			stats.push( cast( screen.fetch("stat" + (i + 1) ), RenderGroupStates ) );
 		
-		popup.fetch("title").setLabel( Player.playerChoice );
+		/*popup.fetch("title").setLabel( Player.playerChoice );
+		hide
 		
-		setStats();
+		setStats();*/
+		switchState( STATS_LOADING );
 	}
 	
 	override private function reset():Void 
 	{
 		super.reset();
-		
-			
+	}
+	
+	override private function handleSwitchState(id:Dynamic):Bool 
+	{
+		switch ( cast( id, PlaySceneState ) ) {
+			case STATS_LOADING:
+				for ( s in stats )
+					s.visible = false;
+				screen.fetch("loading").visible = true;
+				Player.getStats( null, onLoadedStats );
+			case STATS_SHOW:
+				screen.fetch("loading").visible = false;
+				//for ( s in stats )
+				//	s.visible = false;
+		}
+		return true;
 	}
 	
 	override public function update(elapsed:Float):Void 
 	{
 		super.update(elapsed);
 		
+		switch ( cast( state, PlaySceneState ) ) {
+			case STATS_LOADING:
+			case STATS_SHOW:
+		}
+	}
+	
+	private function onLoadedStats():Void {
+		switchState( STATS_SHOW );
 	}
 	
 }
